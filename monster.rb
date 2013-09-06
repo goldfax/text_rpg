4239158d05f70cc2e12af8a8c8f0dd49167c1484
goldfax <nathanield.freed22@gmail.com>
2013-09-05 21:16:42 -0700

    Define how a monster spawns/ fights

--- monster.rb
+++ monster.rb
@@ -7,16 +7,19 @@ class Monster
 			@m_att = 20
 			@p_def = 10
 			@m_def = 2
+			@health = 500
 		elsif @role == 2
 			@p_att = 20
 			@m_att = 10
 			@m_def = 10
 			@p_def = 2
+			@health = 500
 		elsif @role == 3
 			@p_att = 15
 			@m_att = 15
 			@m_def = 6
 			@p_def = 6
+			@health = 500
 		end
 	end
 
