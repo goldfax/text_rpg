fd794f1d42cdab416f64ec617d83cf2ebdd3d3d7
goldfax <nathanield.freed22@gmail.com>
2013-09-09 00:31:12 -0700

    The player needs a list inventory function/char menu function

--- player.rb
+++ player.rb
@@ -1,30 +1,30 @@
 class Player
-	attr_accessor :level, :exp, :name, :gender, :role, :gold
+	attr_accessor :level, :exp, :name, :gender, :role, :gold               #there may be some gender specific actions that one can take
 	
 	def level_up
 		if @exp >= 100
 			@level += 1
 		end
 	end
-
+=begin
 	def fight
 		if adj_monst
 		else
 		end
 	end
 
-	def adj_monst                 #if there is an adjacent monster the 
+	def adj_monst                 #to check if a monster is adjacent when the player is in a field
 		if          
-			true
+			true                  #finish this after I have the map class finished
 		else
 			false
 		end
 	end
 
 	def position
-		
+		git 
 	end
-
+=end
 	def death
 		if @health == 0
 			puts "GAME OVER"
@@ -43,7 +43,7 @@ class Player
 		@gold -= lostgold
 	end
 
-	def stats         // 1 is mage, 2 is warrior, 3 is rogue
+	def stats         # 1 is mage, 2 is warrior, 3 is rogue
 		if @role == 1
 			@p_att = 10
 			@m_att = 20
@@ -51,7 +51,7 @@ class Player
 			@m_def = 2
 			@health = 500
 		elsif @role == 2
-			@p_att = 20
+			@p_att = 20             #stats --- nothing really concrete just made up numbers, maybe should store the stats in a hash
 			@m_att = 10
 			@m_def = 10
 			@p_def = 2
@@ -64,4 +64,7 @@ class Player
 			@health = 500
 		end
 	end
+
+	#def player_menu               #display character stats -- health, patt, matt, pdef, mdef -- also give options to save and exit the game
+	#end
 end
