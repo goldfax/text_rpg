0e89c8c083ab5393e31cdfb52d5bdfbee187db58
goldfax <nathanield.freed22@gmail.com>
2013-09-09 00:29:09 -0700

    Changed map class to initialize array with given height/width

--- map.rb
+++ map.rb
@@ -2,7 +2,7 @@ class Map
 	attr_accessor :region, :name   #map name, and region accessor methods added auto
 	def initialize width height
 		@width = width
-		@length = length
+		@height = height
 		@map = [@height][@width]  #initialize an array with the given height and width parameters
 	end
 end
