9de82aaf93d04275e0fbe97425837f60374d25c2
goldfax <nathanield.freed22@gmail.com>
2013-12-12 20:57:46 -0800

    Update story of the game, and complete the options menu

--- main.rb
+++ main.rb
@@ -30,17 +30,33 @@ def main_menu
 		print "Enter desired name for save file: "
 		filename = gets.chomp
 		save(filename)
-	#elsif choice == 3    #if the choice is three then give an options menu -- add music, graphics?
+	elsif choice == 3    #if the choice is three then give an options menu -- add music, graphics?
+		puts "What would you like to do?"
+		puts "1. Change character name"
+		puts "2. Change character gender"
+		puts "3. Change character class"
+		puts "4. Change character age"
+		option = gets.chomp
+		if option == 1
+			puts "What would you like to change your character's name to?"
+			newchar.name = gets.chomp   
+		elsif option == 2 
+			puts "What would you like to change your character's gender to?"
+			newchar.gender = gets.chomp
+		elsif option == 3
+			puts "What would you like to change your character's role to?"
+			newchar.role = gets.chomp
 	elsif choice == 4    #exit the program with the message goodbye
 		puts "Goodbye"
 		exit
+	else
+		puts "You did not choose the correct option please try again."
+		main_menu
 	end
 end
 
 def save(file)
-	File.open("#{file}.txt", 'w') { |file| file.write("#{file}") }
+	File.open("#{file}.txt", 'w') { |file| file.write("This is a saved game.") }
 end
-
-
 
 main_menu
